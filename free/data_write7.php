ee175d6967200b5bce7b4ccbb911de2daa4838e6
PARKSH <changjo0505@naver.com>
2023-09-17 11:32:01 +0900

    new data_write7 double

--- free/data_write7.php
+++ free/data_write7.php
@@ -1,7 +1,16 @@
 <?php
+ini_set( "display_errors", 1 );
+
 foreach ($_REQUEST as $k => $v) {
 		$$k = hexdec($v);
 }
+
+
+//$file_point = fopen("data_file.txt", "a+");
+//fwrite($file_point, $_SERVER['QUERY_STRING'].'\n');
+//fclose($file_point);
+
+
 
 
 $date = date("Y-m-d");
@@ -17,55 +26,84 @@ $bd_type = substr($bd_num, 0, 1);
 $bd_number = substr($bd_num, 1, 1);
 
 
+// $da7 = dechex($d7);
+// $da8 = dechex($d8);
+
+
 if ($bd_type != 1) {
 
-	if ($address == '999') {
-		$d1 = 0;
-		$d2 = 0;
-	} else if ($address == '998') {
-		$d1 = 0;
-		$d2 = 0;
-	}
 
-	if ($bd_type == 2) {
-		$d1 = round($d1 / 65536 * 175 - 46.85,1);
-		$d2 = round($d2 / 65536 * 125 - 6, 1);
-		$d7 = round($d7 / 1.2, 1);
-	} else if ($bd_type == 3) {
-		$d1 = round($d1 / 10,1);
-		$d2 = round($d2 / 10, 1);
-	} else if ($bd_type == 4) {
-		$d1 = round($d1 / 65536 * 100,1);
-		//$d2 = round(($d2 - 763) / 3332 * 14 , 1);
-	} else if ($bd_type == 5) {
-		$d1 = round($d1 / 10,1);
-		$d2 = round($d2 / 10, 1);
-	}	
+	 if ($address == '999') {
+	 	$d1 = 0;
+	 	$d2 = 0;
+	 } else if ($address == '998') {
+	 	$d1 = 0;
+	 	$d2 = 0;
+	 }
 
-	$sql = "INSERT INTO water.raw_data (`create_at`,`address`,`board_type`,`board_number`,`data1`,`data2`,`data3`,`data4`,`data5`,`data6`,`data7`)
-    		VALUES ('{$create_at}', $address, $bd_type, $bd_number, $d1, $d2, $d3, $d4, $d5, $d6, $d7)";
+	 if ($bd_type == 2) {
+	 	$d8 = 0;
+	 	$d1 = round($d1 / 65536 * 175 - 46.85,1); //온도센서
+	 	$d2 = round($d2 / 65536 * 125 - 6, 1); // 습도센서
+	 	if ($d2>99.9){
+	 		$d2 = 99.9;
+	 	}
+	 	$d7 = round($d7 / 1.2, 1); // 조도센서
+	 } else if ($bd_type == 3) {
+	 	$d8 = 0;
+	 	$d1 = round($d1 / 10,1); //온도센서 AM2305
+	 	$d2 = round($d2 / 10,1); //습도센서 AM2305
+	 } else if ($bd_type == 4) {
+	 	$d8 = 0;
+	 	$d1 = round($d1 / 65536 * 100,1); //지습도 센서
+	 	//$d2 = round(($d2 - 763) / 3332 * 14 , 1);
+		
+	 	if ($d2 < 763){
+	 		$d2 = 0;
+	 	}
+	 	else {
+	 		$d2 = round(($d2 - 763) / 3335 * 2500 , 1); // 401,501 버섯농장 PAR센서
+	 	}
+
+	 	if ($d3 < 763){
+	 		$d3 = 0;
+	 	} else {
+	 	$d3 = round(($d2 - 763) / 3335 * 4000 , 1); // 301 버섯농장 PAR센서
+	 	}
+	
+	 } else if ($bd_type == 5) {
+	 	$d1 = round($d1 / 10,1);
+	 	$d2 = round($d2 / 10,1);
+	 	$d3 = round($d3 / 10,1);
+	 	$d4 = round($d4 / 10,1);
+	 	$d5 = round($d5 / 10,1);
+	 	$d6 = round($d6 / 10,1);
+	 	$d7 = round($d7 / 10,1);
+	 	$d8 = round($d8 / 10,1);
+	 } else if ($bd_type == 6) {
+		$_d7 = substr($_REQUEST['d7'], 3, 2).substr($_REQUEST['d7'], 1, 2);
+		$d7 = $_d7/1000;
+
+		$_d8 = substr($_REQUEST['d8'], 3, 2).substr($_REQUEST['d8'], 1, 2);
+		$d8 = '0'.$_d8;
+
+		$d1 = ($_d8*10)+$d7;
+	 }	 
+
+	$sql = "INSERT INTO raw_data (`create_at`,`address`,`board_type`,`board_number`,`data1`,`data2`,`data3`,`data4`,`data5`,`data6`,`data7`,`data8`)
+    		VALUES ('{$create_at}', $address, $bd_type, $bd_number, $d1, $d2, $d3, $d4, $d5, $d6, $d7, $d8)";
 
 	$result = mysqli_query($conn, $sql);
-}
+	
+	$sql = "INSERT INTO upa.raw_data (`created_at`,`address`,`board_type`,`board_number`,	`data1`,`data2`,`data3`,`data4`,`data5`,`data6`,`data7`,`data8`)
+	VALUES ('{$create_at}', $address, $bd_type, $bd_number, $d1, $d2, $d3, $d4, $d5, $d6, $d7, 0)";
 
-$control_sql = "select * from water.control_data where address = $address and board_type = $bd_type and board_number = $bd_number order by create_at desc limit 1 ";
+  $result = mysqli_query($conn, $sql);
 
-$result = mysqli_query($conn, $control_sql);
-$row = mysqli_fetch_array($result);
+	}
+	
+$textdata = "@".$time."X000Y0000#<br>";
 
-if (isset($row['idx'])) {
-
-	$relay1 = $row['relay1'] == '' ? 0 : $row['relay1'];
-	$relay2 = $row['relay2'] == '' ? 0 : $row['relay2'];
-
-
-	$str = "X000Y".$relay1.$relay2."00#";
-
-	$textdata = "@".$time.$str;
-
-} else {
-	$textdata = "@".$time."X000Y0000#<br>";
-}
 
 
 echo $textdata;
