17b5c989fb2c80dfa585870f33652635bfaf94a8
PSH <changjo0505@naver.com>
2024-03-18 16:11:01 +0900

    30일

--- conf/Ajax_Line_Chart_9.data.php
+++ conf/Ajax_Line_Chart_9.data.php
@@ -5,7 +5,7 @@ $query = "
     SELECT idx, created_at, date_format(created_at, \"%m-%d\") as DF,
         (MAX(IF(board_number=4, data1, NULL)) - MIN(IF(board_number=4, data1, NULL)) ) as daily_5building
     FROM upa.raw_data
-    WHERE  address = '2300' and created_at >= now() - interval 7 day
+    WHERE  address = '2300' and created_at >= now() - interval 30 day
     group by DF
     ORDER BY idx asc;
     "; 
